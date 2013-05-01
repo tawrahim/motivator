964d9a9e848e33e34863c2879c69b48b79d511b3
Tawheed Abdul-Raheem <tawrahim@gmail.com>
2013-05-01 19:46:09 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motivator::Application.config.secret_token = 'b5f21fc4ebc207a160a0818af8a065f84e9d2e9554b3796c3782fab1b01f94a27ac473bdc32f0e7f2dfc9958cea3d0afe825e3964c1ef1ed2009900ac3370f65'
